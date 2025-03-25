1ed34d41113158703aea0e49f09d557f54a581c0
Mukesh <kmukeshbabu982@gmail.com>
2025-03-25 19:16:23 +0530

    Implemented search functionality

--- src/components/RestaurantContainer.jsx
+++ src/components/RestaurantContainer.jsx
@@ -5,6 +5,8 @@ import Shimmer from "./Shimmer";
 
 const RestaurantContainer = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -27,6 +29,7 @@ const RestaurantContainer = () => {
         const restaurants = jsonData?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
         //console.log(restaurants);
         setListOfRestaurants(restaurants);
+        setFilteredRestaurants(restaurants);
 
     } catch (error) {
         console.log(error);
@@ -37,27 +40,44 @@ const RestaurantContainer = () => {
    
   return (listOfRestaurants.length === 0) ? <Shimmer/> : (
     <div className="">
-      <div>
-        <button 
-        className="px-4 py-2 bg-blue-500 text-white hover:bg-blue-700 transition cursor-pointer rounded-lg mx-2 my-2"
-        onClick={()=>{
-          const filteredRestaurants = listOfRestaurants.filter(
-            (restaurant)=> restaurant.info.avgRating > 4
-          );
-          setListOfRestaurants(filteredRestaurants);
-        }}
-        >
-          Top Rated Restaurants
-        </button>
+      <div className="flex items-center gap-10 ">
+        <div>
+          <input type="text" className="border-2 rounded-lg m-2 " value={searchText} onChange={(e)=> setSearchText(e.target.value)}/>
+          <button 
+            className="bg-gray-400 rounded-lg px-4 py-2 m-2 cursor-pointer"
+            onClick={()=>{
+              const searchRestaurants = listOfRestaurants.filter(
+                (res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(searchRestaurants);
+            }} 
+          >
+            Search
+          </button>
+        </div>
+
+        <div>
+          <button 
+          className="px-4 py-2 bg-blue-500 text-white hover:bg-blue-700 transition cursor-pointer rounded-lg mx-2 my-2"
+          onClick={()=>{
+            const filterTopRestaurants = listOfRestaurants.filter(
+              (restaurant)=> restaurant.info.avgRating > 4
+            );
+            setFilteredRestaurants(filterTopRestaurants);
+          }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
-        <div className="flex flex-wrap justify-between">
+      
+      <div className="flex flex-wrap justify-between">
           {
-            listOfRestaurants.map((restaurant)=>(
+            filteredRestaurants.map((restaurant)=>(
               <RestaurantCard key={restaurant.info.id} resData = {restaurant}/>
             ))
-          }
-           
-        </div>
+          }   
+      </div>
       
     </div>
   );
